535b27f1d46ccf1ce820c410f0a1971bfe95b936
Shunnoni <133608095+Shunnoni@users.noreply.github.com>
2024-05-08 18:23:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the dropdown options
+    var option1 = document.getElementById("option1");
+    var option2 = document.getElementById("option2");
+    var option3 = document.getElementById("option3");
 
+    // Add click event listeners to the dropdown options
+    option1.addEventListener("click", function() {
+        alert("Option 1 clicked!");
+        // You can add your desired functionality here
+    });
+
+    option2.addEventListener("click", function() {
+        alert("Option 2 clicked!");
+        // You can add your desired functionality here
+    });
+
+    option3.addEventListener("click", function() {
+        alert("Option 3 clicked!");
+        // You can add your desired functionality here
+    });
+});
